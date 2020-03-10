ebce5eac014d5feb2b8dcba9341cd00ea514d897
tomriddle7 <tomriddle7@naver.com>
2020-03-10 22:51:13 +0900

    Add Header

---
+++ src/Components/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: #221176;
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Developer = styled.a`
+  position: absolute;
+  right: 20px;
+`;
+
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Buy Your Mask</SLink>
+      </Item>
+    </List>
+    
+    <Developer href="https://github.com/tomriddle7/needyourmask" target="_blank">
+      <FontAwesomeIcon icon={faGithub} size="2x"/>
+    </Developer>
+  </Header>
+));
