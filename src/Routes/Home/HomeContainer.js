76bcd7580de400626bd54ffb4fae399dc8f8bc04
tomriddle7 <tomriddle7@naver.com>
2020-03-12 15:20:52 +0900

    Update HomeContainer.js

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -43,73 +43,75 @@ export default class extends React.Component {
         });
         
         //마스크 정보로 마커 생성
-        this.state.maskData.forEach(element => {
-          let imgType, imgStat;
-          switch(element.type) {
-            case "01":
-              imgType = "pill";
-              break;
-            case "02":
-              imgType = "mart";
-              break;
-            case "03":
-              imgType = "post";
-              break;
-          }
-          switch(element.remain_stat) {
-            case "plenty":
-              imgStat = "green";
-              break;
-            case "some":
-              imgStat = "yellow";
-              break;
-            case "few":
-              imgStat = "red";
-              break;
-            case "empty":
-              imgStat = "grey";
-              break;
-            default:
-              imgStat = "grey";
-              break;
-          }
-          let imageSrc = require(`../../Assets/marker_${imgType}_${imgStat}.png`),
-          imageSize = new kakao.maps.Size(20, 20), // 마커이미지의 크기입니다
-          imageOption = {offset: new kakao.maps.Point(10, 10)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-          
-
-          // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-          let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-              markerPosition = new kakao.maps.LatLng(element.lat, element.lng); // 마커가 표시될 위치입니다
+        if(this.state.maskData) {
+          this.state.maskData.forEach(element => {
+            let imgType, imgStat;
+            switch(element.type) {
+              case "01":
+                imgType = "pill";
+                break;
+              case "02":
+                imgType = "mart";
+                break;
+              case "03":
+                imgType = "post";
+                break;
+            }
+            switch(element.remain_stat) {
+              case "plenty":
+                imgStat = "green";
+                break;
+              case "some":
+                imgStat = "yellow";
+                break;
+              case "few":
+                imgStat = "red";
+                break;
+              case "empty":
+                imgStat = "grey";
+                break;
+              default:
+                imgStat = "grey";
+                break;
+            }
+            let imageSrc = require(`../../Assets/marker_${imgType}_${imgStat}.png`),
+            imageSize = new kakao.maps.Size(20, 20), // 마커이미지의 크기입니다
+            imageOption = {offset: new kakao.maps.Point(10, 10)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+            
   
-          // 마커를 생성합니다
-          let marker = new kakao.maps.Marker({
-              position: markerPosition, 
-              image: markerImage // 마커이미지 설정 
-          });
+            // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+            let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                markerPosition = new kakao.maps.LatLng(element.lat, element.lng); // 마커가 표시될 위치입니다
+    
+            // 마커를 생성합니다
+            let marker = new kakao.maps.Marker({
+                position: markerPosition, 
+                image: markerImage // 마커이미지 설정 
+            });
+    
+            // 마커가 지도 위에 표시되도록 설정합니다
+            marker.setMap(map);
+            let iwContent = `<div style="padding:5px; font-size: 15px;">${element.name}<br>입고시간: ${element.stock_at ? element.stock_at.substr(11, 5) : null}<br><a href='https://map.kakao.com/link/search/${element.name}' style="color:blue" target="_blank">판매처정보</a>　<a href='https://map.kakao.com/link/to/${element.name},${element.lat},${element.lng}' style="color:blue" target="_blank">길찾기</a></div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                iwPosition = new kakao.maps.LatLng(element.lat, element.lng); //인포윈도우 표시 위치입니다
   
-          // 마커가 지도 위에 표시되도록 설정합니다
-          marker.setMap(map);
-          let iwContent = `<div style="padding:5px; font-size: 15px;">${element.name}<br>입고시간: ${element.stock_at ? element.stock_at.substr(11, 5) : null}<br><a href='https://map.kakao.com/link/search/${element.name}' style="color:blue" target="_blank">판매처정보</a>　<a href='https://map.kakao.com/link/to/${element.name},${element.lat},${element.lng}' style="color:blue" target="_blank">길찾기</a></div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-              iwPosition = new kakao.maps.LatLng(element.lat, element.lng); //인포윈도우 표시 위치입니다
-
-          // 인포윈도우를 생성합니다
-          let infowindow = new kakao.maps.InfoWindow({
-              position: iwPosition, 
-              content: iwContent,
-              removable: true
+            // 인포윈도우를 생성합니다
+            let infowindow = new kakao.maps.InfoWindow({
+                position: iwPosition, 
+                content: iwContent,
+                removable: true
+            });
+  
+            // 마커에 클릭이벤트를 등록합니다
+            kakao.maps.event.addListener(marker, 'click', function() {
+              // 마커 위에 인포윈도우를 표시합니다
+              infowindow.open(map, marker);  
+            });
+            
+            this.setState({
+              loading: false
+            });
           });
-
-          // 마커에 클릭이벤트를 등록합니다
-          kakao.maps.event.addListener(marker, 'click', function() {
-            // 마커 위에 인포윈도우를 표시합니다
-            infowindow.open(map, marker);  
-          });
-          
-          this.setState({
-            loading: false
-          });
-        });
+        }
       });
     }
   };
